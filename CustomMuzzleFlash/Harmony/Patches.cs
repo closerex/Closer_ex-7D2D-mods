b88213fffd6135dddc570fd58d84a5f202d8f327
C Ivor <56746689+closerex@users.noreply.github.com>
2022-07-10 23:45:16 +0800

    fix existing particle warning

--- CustomMuzzleFlash/Harmony/Patches.cs
+++ CustomMuzzleFlash/Harmony/Patches.cs
@@ -7,19 +7,19 @@ class MuzzlePatch
     [HarmonyPostfix]
     private static void Postfix_ReadFrom_ItemActionAttack(DynamicProperties _props)
     {
-		if (_props.Values.ContainsKey("Particles_muzzle_fire"))
+		if (_props.Values.ContainsKey("Particles_muzzle_fire") && !ParticleEffect.IsAvailable(_props.Values["Particles_muzzle_fire"]))
 		{
 			ParticleEffect.RegisterBundleParticleEffect(_props.Values["Particles_muzzle_fire"]);
 		}
-		if (_props.Values.ContainsKey("Particles_muzzle_fire_fpv"))
+		if (_props.Values.ContainsKey("Particles_muzzle_fire_fpv") && !ParticleEffect.IsAvailable(_props.Values["Particles_muzzle_fire_fpv"]))
 		{
 			ParticleEffect.RegisterBundleParticleEffect(_props.Values["Particles_muzzle_fire_fpv"]);
 		}
-		if (_props.Values.ContainsKey("Particles_muzzle_smoke"))
+		if (_props.Values.ContainsKey("Particles_muzzle_smoke") && !ParticleEffect.IsAvailable(_props.Values["Particles_muzzle_smoke"]))
 		{
 			ParticleEffect.RegisterBundleParticleEffect(_props.Values["Particles_muzzle_smoke"]);
 		}
-		if (_props.Values.ContainsKey("Particles_muzzle_smoke_fpv"))
+		if (_props.Values.ContainsKey("Particles_muzzle_smoke_fpv") && !ParticleEffect.IsAvailable(_props.Values["Particles_muzzle_smoke_fpv"]))
 		{
 			ParticleEffect.RegisterBundleParticleEffect(_props.Values["Particles_muzzle_smoke_fpv"]);
 		}
