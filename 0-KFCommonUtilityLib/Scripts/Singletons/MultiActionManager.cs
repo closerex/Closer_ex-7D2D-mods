70074832a2c9d8d1636d8af4f4a5510dc8172282
C Ivor <closer_ex@outlook.com>
2023-12-14 01:40:53 +0800

    mapping stuff

--- 0-KFCommonUtilityLib/Scripts/Singletons/MultiActionManager.cs
+++ 0-KFCommonUtilityLib/Scripts/Singletons/MultiActionManager.cs
@@ -11,9 +11,9 @@ namespace KFCommonUtilityLib.Scripts.Singletons
     //concept: maintain an entityID-AltActionIndice mapping on both server and client
     //and get the correct action before calling ItemAction.*
     //always set MinEventParams.itemActionData
-    //set meta and ammoindex on switching mode, keep current mode in metadata
+    //done: set meta and ammoindex on switching mode, keep current mode in metadata
     //should take care of accuracy updating
-    //should support shared meta
+    //partially done: should support shared meta
     //alt actions should be considered primary, redirect index == 0 to custom method
     //redirect ItemClass.Actions[0] to custom method
     //patch GameManager.updateSendClientPlayerPositionToServer to sync data, so that mode change always happens after holding item change
@@ -21,20 +21,21 @@ namespace KFCommonUtilityLib.Scripts.Singletons
     public struct MultiActionIndice
     {
         public const int MAX_ACTION_COUNT = 3;
-        public unsafe fixed int indice[MAX_ACTION_COUNT];
+        public unsafe fixed int indices[MAX_ACTION_COUNT];
         public unsafe fixed int metaIndice[MAX_ACTION_COUNT];
+        public readonly int modeCount;
 
         //this should only be called in createModifierData
         public unsafe MultiActionIndice(ItemAction[] actions)
         {
-            indice[0] = 0;
+            indices[0] = 0;
             metaIndice[0] = 0;
             int last = 1;
             for (int i = 3; i < actions.Length && last < MAX_ACTION_COUNT; i++)
             {
                 if (actions[i] != null)
                 {
-                    indice[last] = i;
+                    indices[last] = i;
                     if (actions[i].Properties.Values.TryGetString("ShareMetaWith", out string str) && int.TryParse(str, out int shareWith))
                     {
                         metaIndice[last] = shareWith;
@@ -46,15 +47,23 @@ namespace KFCommonUtilityLib.Scripts.Singletons
                     last++;
                 }
             }
+            modeCount = last;
+            for (; last < MAX_ACTION_COUNT; last++)
+            {
+                indices[last] = -1;
+                metaIndice[last] = -1;
+            }
         }
     }
 
     //MultiActionMapping instance should be changed on ItemAction.StartHolding, so we only need to send curIndex.
     public class MultiActionMapping
     {
+        public const string STR_MULTI_ACTION_INDEX = "MultiActionIndex";
         public readonly MultiActionIndice mapping;
         private int curIndex;
         public ItemValue itemValue;
+        public string toggleSound;
 
         /// <summary>
         /// when set CurIndex from local input, also set manager to dirty to update the index on other clients
@@ -64,25 +73,92 @@ namespace KFCommonUtilityLib.Scripts.Singletons
             get => curIndex;
             set
             {
-                curIndex = value;
-                itemValue.SetMetadata("MultiActionIndex", curIndex, TypedMetadataValue.TypeTag.Integer);
-                //also set meta and ammo index
+                unsafe
+                {
+                    if (curIndex == value)
+                        return;
+                    //mostly for CurIndex++, cycle through available indices
+                    if (value >= MultiActionIndice.MAX_ACTION_COUNT || mapping.indices[value] == -1)
+                        value = 0;
+                    //save previous meta and ammo index to metadata
+                    int curMetaIndex = CurMetaIndex;
+                    itemValue.SetMetadata(MultiActionUtils.ActionMetaNames[curMetaIndex], itemValue.Meta, TypedMetadataValue.TypeTag.Integer);
+                    itemValue.SetMetadata(MultiActionUtils.ActionSelectedAmmoNames[curMetaIndex], itemValue.SelectedAmmoTypeIndex, TypedMetadataValue.TypeTag.Integer);
+
+                    //load current meta and ammo index from metadata
+                    curIndex = value;
+                    curMetaIndex = CurMetaIndex;
+                    itemValue.SetMetadata(STR_MULTI_ACTION_INDEX, value, TypedMetadataValue.TypeTag.Integer);
+                    object res = itemValue.GetMetadata(MultiActionUtils.ActionMetaNames[curMetaIndex]);
+                    if (res is false || res is null)
+                    {
+                        itemValue.SetMetadata(MultiActionUtils.ActionMetaNames[curMetaIndex], 0, TypedMetadataValue.TypeTag.Integer);
+                        itemValue.Meta = 0;
+                    }
+                    else
+                    {
+                        itemValue.Meta = (int)res;
+                    }
+                    res = itemValue.GetMetadata(MultiActionUtils.ActionSelectedAmmoNames[curMetaIndex]);
+                    if (res is false || res is null)
+                    {
+                        itemValue.SetMetadata(MultiActionUtils.ActionSelectedAmmoNames[curMetaIndex], 0, TypedMetadataValue.TypeTag.Integer);
+                        itemValue.SelectedAmmoTypeIndex = 0;
+                    }
+                    else
+                    {
+                        itemValue.SelectedAmmoTypeIndex = (byte)res;
+                    }
+                }
             }
         }
 
-        public MultiActionMapping(MultiActionIndice mapping, ItemValue itemValue)
+        //for ItemClass.Actions access
+        public int CurMode
+        {
+            get
+            {
+                unsafe
+                {
+                    return mapping.indices[curIndex];
+                }
+            }
+        }
+
+        //for meta saving on mode switch only?
+        public int CurMetaIndex
+        {
+            get
+            {
+                unsafe
+                {
+                    return mapping.metaIndice[curIndex];
+                }
+            }
+        }
+
+        public int ModeCount => mapping.modeCount;
+
+        //while mapping object is changed on StartHolding, it's initialized on createModifierData
+        //we set the curIndex field instead of the property, according to following situations:
+        //1. it's a newly created ItemValue, meta and ammo index belongs to action0, no saving is needed;
+        //2. it's an existing ItemValue, meta and ammo index is set to its action index, still saving is unnecessary.
+        public MultiActionMapping(MultiActionIndice mapping, ItemValue itemValue, string toggleSound)
         {
             this.mapping = mapping;
             this.itemValue = itemValue;
-            if (itemValue.HasMetadata("MultiActionIndex", TypedMetadataValue.TypeTag.Integer))
+            object res = itemValue.GetMetadata(STR_MULTI_ACTION_INDEX);
+            if (res is false || res is null)
             {
-                curIndex = (int)itemValue.GetMetadata("MultiActionIndex");
+                itemValue.SetMetadata(STR_MULTI_ACTION_INDEX, 0, TypedMetadataValue.TypeTag.Integer);
+                curIndex = 0;
             }
             else
             {
-                itemValue.SetMetadata("MultiActionIndex", 0, TypedMetadataValue.TypeTag.Integer);
-                curIndex = 0;
+                curIndex = (int)res;
             }
+
+            this.toggleSound = toggleSound;
         }
     }
 
@@ -90,6 +166,38 @@ namespace KFCommonUtilityLib.Scripts.Singletons
     {
         //clear on game load
         private static readonly Dictionary<int, MultiActionMapping> dict_mappings = new Dictionary<int, MultiActionMapping>();
+        //should set to true when:
+        //mode switch input received;
+        //start holding new multi action weapon.
+        //if true, send local curIndex to other clients in updateSendClientPlayerPositionToServer.
+        public static bool LocalModeChanged { get; set; }
+
+        public static void ToggleLocalActionIndex()
+        {
+            EntityPlayerLocal player = GameManager.Instance.World.GetPrimaryPlayer();
+            if (player == null || !dict_mappings.TryGetValue(player.entityId, out MultiActionMapping mapping))
+                return;
+
+            if (mapping.ModeCount <= 1)
+                return;
+            mapping.CurIndex++;
+            player.PlayOneShot(mapping.toggleSound);
+            LocalModeChanged = true;
+        }
+
+        public static int GetActionIndexForEntity(int entityID)
+        {
+            if (!dict_mappings.TryGetValue(entityID, out var mapping) || mapping == null)
+                return 0;
+            return mapping.CurMode;
+        }
+
+        public static int GetMetaIndexForEntity(int entityID)
+        {
+            if (!dict_mappings.TryGetValue(entityID, out var mapping) || mapping == null)
+                return 0;
+            return mapping.CurMetaIndex;
+        }
 
         public static void SetupRadial(XUiC_Radial _xuiRadialWindow, EntityPlayerLocal _epl)
         {
