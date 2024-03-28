072db8dbd9936e212ad28cdb6a42970c51d37f9e
C Ivor <closer_ex@outlook.com>
2024-03-28 17:08:08 +0800

    add new script to change muzzle position

---
+++ 0-KFCommonUtilityLib/KFAttached/KFUtilAttached/MuzzlePositionBinding.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[AddComponentMenu("Muzzle Position Binding")]
+public class MuzzlePositionBinding : MonoBehaviour
+{
+    [SerializeField]
+    private Transform muzzleTrans;
+    [SerializeField]
+    private Vector3 newMuzzlePosition;
+    private Vector3 initialPosition;
+    private void Awake()
+    {
+        if (muzzleTrans)
+            initialPosition = transform.localPosition;
+    }
+
+    private void OnEnable()
+    {
+        if (muzzleTrans)
+            muzzleTrans.localPosition = newMuzzlePosition;
+    }
+
+    private void OnDisable()
+    {
+        if (muzzleTrans)
+            muzzleTrans.localPosition = initialPosition;
+    }
+}
