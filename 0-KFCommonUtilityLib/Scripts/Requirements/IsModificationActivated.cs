a4f2a46f811316d2b9580b17e8eb267961ede017
C Ivor <closer_ex@outlook.com>
2024-05-04 15:52:13 +0800

    add requirement to check weapon mod activation state

---
+++ 0-KFCommonUtilityLib/Scripts/Requirements/IsModificationActivated.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+public class IsModificationActivated : RequirementBase
+{
+    private string modName;
+    private int modId = -1;
+
+    public override bool IsValid(MinEventParams _params)
+    {
+        if (!base.IsValid(_params))
+            return false;
+
+        if (modId < 0)
+        {
+            modId = ItemClass.GetItemClass(modName)?.Id ?? -1;
+            if (modId < 0)
+                return false;
+        }
+
+        if(_params.ItemValue != null)
+        {
+            if (_params.ItemValue.Modifications != null)
+            {
+                foreach (var mod in _params.ItemValue.Modifications)
+                {
+                    if (mod != null && mod.GetItemOrBlockId() == modId && mod.Activated > 0)
+                    {
+                        return !invert;
+                    }
+                }
+            }
+
+            if (_params.ItemValue.CosmeticMods != null)
+            {
+                foreach (var cos in _params.ItemValue.CosmeticMods)
+                {
+                    if (cos != null && cos.GetItemOrBlockId() == modId && cos.Activated > 0)
+                    {
+                        return !invert;
+                    }
+                }
+            }
+        }
+
+        return invert;
+    }
+
+    public override bool ParseXAttribute(XAttribute _attribute)
+    {
+        if (base.ParseXAttribute(_attribute))
+            return true;
+
+        switch (_attribute.Name.LocalName)
+        {
+            case "name":
+                modName = _attribute.Value;
+                return true;
+        }
+
+        return false;
+    }
+}
