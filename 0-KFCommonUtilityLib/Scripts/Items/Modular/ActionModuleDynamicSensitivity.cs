ee7d19a6ac1f73b3717fe0c23fbe040480af6bb5
C Ivor <closer_ex@outlook.com>
2024-12-17 15:43:26 +0800

    add support for dynamic aim mouse sensitivity

---
+++ 0-KFCommonUtilityLib/Scripts/Items/Modular/ActionModuleDynamicSensitivity.cs
@@ -0,0 +1,50 @@
+﻿using KFCommonUtilityLib.Scripts.Attributes;
+using KFCommonUtilityLib.Scripts.StaticManagers;
+using UnityEngine;
+
+[TypeTarget(typeof(ItemActionZoom), typeof(DynamicSensitivityData))]
+public class ActionModuleDynamicSensitivity
+{
+    [MethodTargetPostfix(nameof(ItemAction.AimingSet))]
+    private void Postfix_AimingSet(ItemActionData _actionData, bool _isAiming, bool _wasAiming, DynamicSensitivityData __customData)
+    {
+        float originalSensitivity = GamePrefs.GetFloat(EnumGamePrefs.OptionsZoomSensitivity);
+        if (_isAiming)
+        {
+            PlayerMoveController.Instance.mouseZoomSensitivity = originalSensitivity / Mathf.Sqrt(__customData.variableZoomData == null ? __customData.zoomRatio : __customData.variableZoomData.curScale);
+        }
+        else
+        {
+            PlayerMoveController.Instance.mouseZoomSensitivity = originalSensitivity;
+        }
+    }
+
+    [MethodTargetPostfix(nameof(ItemAction.OnModificationsChanged))]
+    private void Postfix_OnModificationsChanged(ItemActionZoom __instance, ItemActionData _data, DynamicSensitivityData __customData)
+    {
+        if (_data is IModuleContainerFor<ActionModuleVariableZoom.VariableZoomData> variableZoomData)
+        {
+            __customData.variableZoomData = variableZoomData.Instance;
+        }
+        else
+        {
+            string str = __instance.Properties.GetString("ZoomRatio");
+            if (string.IsNullOrEmpty(str))
+            {
+                str = "1";
+            }
+            __customData.zoomRatio = StringParsers.ParseFloat(_data.invData.itemValue.GetPropertyOverride("ZoomRatio", str));
+        }
+    }
+
+    public class DynamicSensitivityData
+    {
+        public ActionModuleVariableZoom.VariableZoomData variableZoomData = null;
+        public float zoomRatio = 1.0f;
+
+        public DynamicSensitivityData(ItemInventoryData _invData, int _indexInEntityOfAction, ActionModuleDynamicSensitivity _module)
+        {
+
+        }
+    }
+}
