61493fcbd098d3ca9afc465d3049fb950533b5cd
C Ivor <closer_ex@outlook.com>
2025-02-03 16:17:17 +0800

    fix tagged module not properly handling multiple tags append/remove

--- 0-KFCommonUtilityLib/Scripts/Items/Modular/ActionModuleTagged.cs
+++ 0-KFCommonUtilityLib/Scripts/Items/Modular/ActionModuleTagged.cs
@@ -9,12 +9,13 @@ public class ActionModuleTagged
     private void Postfix_OnModificationChanged(ItemAction __instance, ItemActionData _data, TaggedData __customData)
     {
         var tags = __instance.Properties.GetString("ActionTags").Split(',');
-        var tags_to_add = _data.invData.itemValue.GetAllPropertyOverridesForAction("ActionTagsAppend", __instance.ActionIndex);
-        var tags_to_remove = _data.invData.itemValue.GetAllPropertyOverridesForAction("ActionTagsRemove", __instance.ActionIndex);
+        var tags_to_add = _data.invData.itemValue.GetAllPropertyOverridesForAction("ActionTagsAppend", __instance.ActionIndex).SelectMany(s => s.Split(','));
+        var tags_to_remove = _data.invData.itemValue.GetAllPropertyOverridesForAction("ActionTagsRemove", __instance.ActionIndex).SelectMany(s => s.Split(','));
         var tags_result = tags.Union(tags_to_add);
         tags_result = tags_result.Except(tags_to_remove);
 
         __customData.tags = tags_result.Any() ? FastTags<TagGroup.Global>.Parse(string.Join(",", tags_result)) : FastTags<TagGroup.Global>.none;
+        //Log.Out($"tags: {string.Join(",", tags_result)}");
     }
 
     public class TaggedData
