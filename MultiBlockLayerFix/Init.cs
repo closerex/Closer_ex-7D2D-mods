898a1b5e82f4bd34f6af6f88ff0c99f19b96f58e
C Ivor <closer_ex@outlook.com>
2025-07-28 18:08:25 +0800

    add new mod

---
+++ MultiBlockLayerFix/Init.cs
@@ -0,0 +1,84 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UniLinq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace MultiBlockLayerFix
+{
+    public class Init : IModApi
+    {
+        private static bool inited = false;
+        public void InitMod(Mod _modInstance)
+        {
+            if (inited)
+            {
+                return;
+            }
+            inited = true;
+            Log.Out(" Loading Patch: " + GetType());
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch]
+    public static class Patches
+    {
+        [HarmonyPatch(typeof(Block), nameof(Block.Init))]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> Transpiler_Block_Init(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var codes = instructions.ToList();
+
+            var mtd_trim = AccessTools.Method(typeof(string), nameof(string.Trim), new System.Type[] { });
+            var ctor_vec3i = AccessTools.Constructor(typeof(Vector3i), new System.Type[] { typeof(int), typeof(int), typeof(int) });
+
+            for (int i = 5; i < codes.Count; i++)
+            {
+                if (codes[i].Calls(mtd_trim) && codes[i - 5].opcode == OpCodes.Ldloc_S)
+                {
+                    var lbd_x = generator.DeclareLocal(typeof(int));
+                    var lbd_z = generator.DeclareLocal(typeof(int));
+
+                    for (int j = i + 1; j < codes.Count; j++)
+                    {
+                        if (codes[j].opcode == OpCodes.Newobj && ctor_vec3i.Equals(codes[j].operand))
+                        {
+                            //Log.Out($"opcode {codes[j - 6].opcode} {codes[j - 6].opcode.OperandType}");
+                            codes[j - 6].operand = (sbyte)'x';
+                            codes.InsertRange(j, new[]
+                            {
+                                new CodeInstruction(OpCodes.Ldloc_S, lbd_z),
+                                new CodeInstruction(OpCodes.Sub)
+                            });
+                            codes.InsertRange(j - 2, new[]
+                            {
+                                new CodeInstruction(OpCodes.Ldloc_S, lbd_x),
+                                new CodeInstruction(OpCodes.Sub)
+                            });
+                            break;
+                        }
+                    }
+
+                    codes.InsertRange(i + 2, new[]
+                    {
+                        new CodeInstruction(OpCodes.Ldloc_S, 7).WithLabels(codes[i + 2].ExtractLabels()),
+                        CodeInstruction.LoadField(typeof(Vector3i), nameof(Vector3i.x)),
+                        new CodeInstruction(OpCodes.Ldc_I4_2),
+                        new CodeInstruction(OpCodes.Div),
+                        new CodeInstruction(OpCodes.Stloc_S, lbd_x),
+                        new CodeInstruction(OpCodes.Ldloc_S, 7),
+                        CodeInstruction.LoadField(typeof(Vector3i), nameof(Vector3i.z)),
+                        new CodeInstruction(OpCodes.Ldc_I4_2),
+                        new CodeInstruction(OpCodes.Div),
+                        new CodeInstruction(OpCodes.Stloc_S, lbd_z),
+                    });
+                    break;
+                }
+            }
+
+            return codes;
+        }
+    }
+}
---
+++ TrueNoRotation/Init.cs
@@ -0,0 +1,110 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using UniLinq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace TrueNoRotation
+{
+    public class Init : IModApi
+    {
+        private static bool inited = false;
+        public void InitMod(Mod _modInstance)
+        {
+            if (inited)
+            {
+                return;
+            }
+            inited = true;
+            Log.Out(" Loading Patch: " + GetType());
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch]
+    public static class Patches
+    {
+        [HarmonyPatch(typeof(Block), nameof(Block.OnBlockPlaceBefore))]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> Transpiler_Block_OnBlockPlaceBefore(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var codes = instructions.ToList();
+
+            for (int i = 0; i < codes.Count - 1; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldloc_S && ((LocalBuilder)codes[i].operand).LocalIndex == 4 && codes[i + 1].Branches(out _))
+                {
+                    codes.InsertRange(i, new[]
+                    {
+                        new CodeInstruction(OpCodes.Ldloc_0).WithLabels(codes[i].ExtractLabels()),
+                        new CodeInstruction(OpCodes.Ldloc_S, 4),
+                        CodeInstruction.CallClosure<Func<Block, bool, bool>>(static (block, flag) =>
+                        {
+                            if (flag || block.AllowedRotations == EBlockRotationClasses.None)
+                            {
+                                //Log.Out($"{block.blockName} allowed rotations {block.AllowedRotations.ToString()} flag {flag}");
+                                return true;
+                            }
+                            return false;
+                        }),
+                        new CodeInstruction(OpCodes.Stloc_S, 4)
+                    });
+                    i += 4;
+                }
+                else if (codes[i].opcode == OpCodes.Stloc_3)
+                {
+                    codes.InsertRange(i, new[]
+                    {
+                        new CodeInstruction(OpCodes.Ldloc_0),
+                        CodeInstruction.CallClosure<Func<int, Block, int>>(static (rnd, block) =>
+                        {
+                            if (block.AllowedRotations == EBlockRotationClasses.None)
+                            {
+                                //Log.Out($"{block.blockName} has no allowed rotations, setting random rotation to 0");
+                                return 0;
+                            }
+                            return rnd;
+                        })
+                    });
+                    i += 2;
+                }
+            }
+            return codes;
+        }
+
+        [HarmonyPatch(typeof(Block), nameof(Block.RotateHoldingBlock))]
+        [HarmonyPrefix]
+        private static bool Prefix_Block_RotateHoldingBlock(Block __instance)
+        {
+            if (__instance.AllowedRotations == EBlockRotationClasses.None)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(BlockPlacement), nameof(BlockPlacement.OnPlaceBlock))]
+        [HarmonyPostfix]
+        private static void Postfix_BlockPlacement_OnPlaceBlock(BlockValue _bv, ref BlockPlacement.Result __result)
+        {
+            var block = _bv.Block;
+            if (block.AllowedRotations == EBlockRotationClasses.None)
+            {
+                __result.blockValue.rotation = 0;
+            }
+        }
+
+        [HarmonyPatch(typeof(BlockPlacement), nameof(BlockPlacement.LimitRotation))]
+        [HarmonyPostfix]
+        private static void Postfix_BlockPlacement_LimitRotation(BlockValue _bv, ref byte __result)
+        {
+            var block = _bv.Block;
+            if (block.AllowedRotations == EBlockRotationClasses.None)
+            {
+                __result = 0;
+            }
+        }
+    }
+}
