59bdd99802f7def1c3962b2905d0c0b278bd34fc
C Ivor <closer_ex@outlook.com>
2025-10-04 22:52:48 +0800

    add new patch

---
+++ RainstormPatches/Init.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using KFCommonUtilityLib.KFAttached.Render;
+using Rainstorm;
+using UnityEngine;
+
+namespace RainstormPatches
+{
+    public class Init : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            Log.Out("Loading KFLib RainStorm Patch");
+            // Register the patch
+            Harmony harmony = new Harmony("com.example.rainstorm.patch");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch]
+    public static class MagnifyScopePatches
+    {
+        [HarmonyPatch(typeof(MagnifyScope), "CreateCamera")]
+        [HarmonyPostfix]
+        private static void Postfix_MagnifyScope_CreateCamera(MagnifyScope __instance, Camera ___pipCamera)
+        {
+            if (___pipCamera)
+            {
+                ___pipCamera.gameObject.GetOrAddComponent<RainstormRenderer>();
+            }
+        }
+    }
+}
